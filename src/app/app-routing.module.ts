d2f70d8eaadecaa8287f10f7e22f057c032d481e
Mason Hirst <mhirstdev@gmail.com>
2023-07-13 20:20:01 -0600

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { WorkHistoryComponent } from './work-history/work-history.component';
 
 const routes: Routes = [
-  // { path: '', component: LandingPageComponent },
-  // { path: 'projects', component: ProjectsPageComponent },
-  // { path: '**', redirectTo: '' },
+  { path: '', component: LandingPageComponent },
+  { path: 'projects', component: ProjectsPageComponent },
+  { path: 'work-history', component: WorkHistoryComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
