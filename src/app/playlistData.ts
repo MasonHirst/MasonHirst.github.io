bdb7bbc1ed008c6ef1f564e577ee8c1162b597ea
Mason Hirst <mason.j.hirst@gmail.com>
2023-08-13 21:01:30 -0600

    added a link from the pullout to go to the audio library

--- src/app/playlistData.ts
+++ src/app/playlistData.ts
@@ -25,7 +25,7 @@ export const playlist: Audio[] = [
   new Audio(
     'https://res.cloudinary.com/dk9vsivmu/video/upload/v1691598962/Dona_Nobis_Pachem_Max_and_Mason_ua0hlu.wav',
     'Dona Nobis Pacem',
-    'Dona nobis pacem is a song with Latin text, often sung as a canon. The words, which mean "Grant us peace", come from the Latin Mass. The first 3 verses are sung my Max Hirst, the 4th by Mason Hirst.',
+    'Dona nobis pacem is a song with Latin text, often sung as a canon. The words, which mean "Grant us peace", come from the Latin Mass. The first 3 verses are sung by Max Hirst, the 4th by Mason Hirst.',
     1,
     2.2,
     '1:05',
@@ -36,7 +36,7 @@ export const playlist: Audio[] = [
   new Audio(
     'https://res.cloudinary.com/dk9vsivmu/video/upload/v1691598964/For_Your_Eyes_Only_m859dy.mp3',
     'For Your Eyes Only',
-    'description',
+    'A cover of the original song by One Direction. Performed by Max Hirst, with Mason Hirst playing the piano and mixing the audio.',
     1,
     .93,
     '4:03',
@@ -47,7 +47,7 @@ export const playlist: Audio[] = [
   new Audio(
     'https://res.cloudinary.com/dk9vsivmu/video/upload/v1691598969/Home_Depot_remixed_ad_ek0qvi.mp3',
     'Home Depot Remixed Ad',
-    'description',
+    'A reimagining of the Home Depot ad, with added elements by Mason Hirst',
     1,
     1.05,
     '2:39',
@@ -58,7 +58,7 @@ export const playlist: Audio[] = [
   new Audio(
     'https://res.cloudinary.com/dk9vsivmu/video/upload/v1691629757/Hallelujah_instrumental_ZOOM_20June2020_kdntai.mp3',
     'Hallelujah - guitar verse',
-    'description',
+    'A guitar rendition of Hallelujah (one verse), looped on a handheld microphone by Mason Hirst',
     1,
     1.8,
     '1:02',
@@ -69,7 +69,7 @@ export const playlist: Audio[] = [
   new Audio(
     'https://res.cloudinary.com/dk9vsivmu/video/upload/v1691629758/Hallelujah_guitar_Zoom_28June2020_jxdgka.mp3',
     'Hallelujah - guitar instrumental',
-    'description',
+    'A longer guitar accompaniment of Hallelujah, looped on a handheld microphone by Mason Hirst',
     1,
     2.8,
     '5:07',
@@ -80,7 +80,7 @@ export const playlist: Audio[] = [
   new Audio(
     'https://res.cloudinary.com/dk9vsivmu/video/upload/v1691629758/ZOOM_Photograph_jxpoxh.mp3',
     'Photograph - piano cover',
-    'description',
+    'A piano cover of Photograph by Ed Sheeran, performed by Mason Hirst',
     1,
     1,
     '3:52',
@@ -91,7 +91,7 @@ export const playlist: Audio[] = [
   new Audio(
     'https://res.cloudinary.com/dk9vsivmu/video/upload/v1691629758/ZOOM_River_flows_in_you_bjlquc.mp3',
     'River Flows in You - piano cover',
-    'description',
+    'A piano cover of River Flows in You by Yiruma, performed by Mason Hirst',
     1,
     1.8,
     '2:39',
